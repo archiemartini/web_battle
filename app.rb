465584ea224e824d47237f09642e67661460daf4
Archie <archieparchie@Archies-Air.home>
2022-05-06 17:36:16 +0100

    27 finito, skinny'd up the controler BIG TIME, looking slick, new global variable game, self explanatory have a look

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/player.rb'
 require './lib/game.rb'
-#here we've required our new tested player.rb, 
-#v simple name initialization situation :p
+
 
 class Battle < Sinatra::Base
   configure :development do
@@ -17,26 +16,20 @@ get '/' do
 end
 
 post '/names' do
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name])
+  player_1 = Player.new(params[:player_1_name])
+  player_2 = Player.new(params[:player_2_name])
+  $game = Game.new(player_1, player_2)
   redirect '/play'
 end
-#now we've replaced our session memory stores and replaced them with instantiations
-#of the new Player class, taking the params of previous as arguments
-
 
 get '/play' do
-  @player_1 = $player_1
-  @player_2 = $player_2
+  @game = $game
   erb :play
 end
-#these global variables go on to define our instance variables
-#required by the /play erb
 
 get '/attack' do
-  @player_1 = $player_1
-  @player_2 = $player_2
-  Game.new.attack(@player_2)
+  @game = $game
+  @game.attack(@game.player_2)
   erb :attack
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,27 @@
 require 'game'
 
 describe Game do
+  let(:player_1) { double("Dusty")}
+  let(:player_2) { double("Haribo")}
+  subject(:game) { Game.new(player_1, player_2)}
 
   it 'has attack method' do
     player_dbl = double('Haribo')
     expect(player_dbl).to receive(:received_damage)
-    subject.attack(player_dbl)
+    game.attack(player_dbl)
+  end
 
+  it 'has #player_1 method' do
+    expect(game.player_1).to eq player_1
+  end
+
+  it 'has #player_2 method' do
+    expect(game.player_2).to eq player_2
+  end
+
+
+  it 'when initialized with correct arguments' do
+    expect(Game.new('Dusty', 'Haribo')).to be_a(Game) 
   end
 
 end
